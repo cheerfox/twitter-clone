217d5af4b134d69a4e4d66d0a5e0a3876a166895
cheerfox <cheerfox@users.noreply.github.com>
2015-12-25 14:50:10 +0800

    1. Finish the function for create a new status 2. add some partial like _top_nav, errors to clean up the views

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,26 @@
+class StatusesController < ApplicationController
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    user = User.first
+    @status.creator = user  ##Update after Authentiction
+    if @status.save
+      flash[:notice] = "Successfully Create a Status!!"
+      redirect_to user_path(user.username)
+    else 
+      render 'new'
+    end
+        
+  end
+
+
+
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :statuses, only: [:new, :create]
   get '/:username', to: "users#show", as: "user"
 end

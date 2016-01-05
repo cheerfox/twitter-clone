c841952d48c3137bf6aa61c576b36a88bd3b263c
cheerfox <cheerfox@users.noreply.github.com>
2016-01-05 10:43:50 +0800

    1. add root path to display all users 2. add require_user to prevent unlogged user to issue a request to some url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "users#index"
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy", as: "logout"
+
   resources :statuses, only: [:new, :create]
   resources :users, only: [:new, :create]
   get '/:username', to: "users#show", as: "user"

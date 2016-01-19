32d8792ce677e22aa7d51e7d33808d6d743659b9
cheerfox <cheerfox@users.noreply.github.com>
2016-01-20 01:24:19 +0800

    fix version

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/timeline', to: "users#timeline"
   get '/mentions', to: "users#mentions"
 
-
+  resources :hashtags, only: [:show]
   resources :statuses, only: [:new, :create]
   resources :users, only: [:new, :create] do
     member do
